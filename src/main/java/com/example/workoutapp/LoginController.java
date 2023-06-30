80f24c6eb4e7c36ec1d8891b54cc4bfbfefeaa55
Aydan Bondurant <AydanBondurant@gmail.com>
2023-06-29 22:26:49 -0400

    first commit

---
+++ src/main/java/com/example/workoutapp/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.workoutapp;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    public Label usernameLabel;
+    public TextField usernameTextfield;
+    public Label passwordLabel;
+    public TextField passwordTextfield;
+    public Button loginBtn;
+}
